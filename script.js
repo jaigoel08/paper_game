fee67c07a815f3cace1266fbaa6be3c410a64a08
jaigoel08 <jaigoel14@gmail.com>
2024-07-21 14:33:22 +0530

    Add files via upload
    This is my rock paper and scissor game

---
+++ script.js
@@ -0,0 +1,49 @@
+function compChoice(){
+    let x = Math.floor(Math.random()*3);
+    if(x===0){
+        return '✊';
+    } else if( x===1){
+        return '📃';
+    } else{
+        return '✂️'
+    }
+}
+function ifRock(){
+    document.querySelector('#user-choice').innerHTML = 'YOU HAVE SELECTED ✊';
+    let computerChoice = compChoice();
+    document.querySelector('#comp-choice').innerHTML = `COMPUTER HAS SELECTED ${computerChoice} `;
+    if(computerChoice === '✊') {
+        document.querySelector('#result').innerHTML = `It's a tie 🤝...`;
+    }
+    else if(computerChoice === '📃') {
+        document.querySelector('#result').innerHTML = `Computer won this time 😟...`;
+    } else {
+        document.querySelector('#result').innerHTML = `Congrats You won 🤩...`;
+    }
+}
+function ifPaper(){
+    document.querySelector('#user-choice').innerHTML = 'YOU HAVE SELECTED 📃';
+    let computerChoice = compChoice();
+    document.querySelector('#comp-choice').innerHTML = `COMPUTER HAS SELECTED ${computerChoice} `;
+    if(computerChoice === '📃') {
+        document.querySelector('#result').innerHTML = `It's a tie 🤝...`;
+    }
+    else if(computerChoice === '✂️') {
+        document.querySelector('#result').innerHTML = `Computer won this time 😟...`;
+    } else {
+        document.querySelector('#result').innerHTML = `Congrats You won 🤩...`;
+    }
+}
+function ifScissor(){
+    document.querySelector('#user-choice').innerHTML = 'YOU HAVE SELECTED ✂️';
+    let computerChoice = compChoice();
+    document.querySelector('#comp-choice').innerHTML = `COMPUTER HAS SELECTED ${computerChoice} `;
+    if(computerChoice === '✂️') {
+        document.querySelector('#result').innerHTML = `It's a tie 🤝...`;
+    }
+    else if(computerChoice === '✊') {
+        document.querySelector('#result').innerHTML = `Computer won this time 😟...`;
+    } else {
+        document.querySelector('#result').innerHTML = `Congrats You won 🤩...`;
+    }
+}
